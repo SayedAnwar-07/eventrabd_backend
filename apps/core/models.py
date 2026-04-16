f6dd5c1b30db1e3f37ed388b89009946782bc0a0
SayedAnwarOfficial <sayedanwarofficial@gmail.com>
2026-04-16 17:10:08 +0600

    bson delete

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,11 +1,28 @@
+import time
+import random
+import threading
+
 from django.db import models
 from django.utils.text import slugify
-from bson.objectid import ObjectId
 from django.utils import timezone
 
 
+_counter = random.randint(0, 0xFFFFFF)
+_counter_lock = threading.Lock()
+
+
 def generate_bson_id():
-    return str(ObjectId())
+    global _counter
+
+    timestamp_hex = f"{int(time.time()):08x}"
+    random_hex = f"{random.getrandbits(40):010x}"
+
+    with _counter_lock:
+        _counter = (_counter + 1) % 0xFFFFFF
+        counter_hex = f"{_counter:06x}"
+
+    return f"{timestamp_hex}{random_hex}{counter_hex}"
+
 
 class UIDMixin(models.Model):
     id = models.CharField(
@@ -13,11 +30,12 @@ class UIDMixin(models.Model):
         max_length=24,
         default=generate_bson_id,
         editable=False,
-        unique=True
+        unique=True,
     )
 
     class Meta:
         abstract = True
+
 
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
@@ -26,10 +44,8 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
-# Slug utility
-def unique_slugify(instance, value, slug_field_name='slug'):
-    from django.utils.text import slugify
 
+def unique_slugify(instance, value, slug_field_name="slug"):
     slug = slugify(value)
     ModelClass = instance.__class__
     qs = ModelClass.objects.exclude(id=instance.id)
