ed4d839ed49f339e4b69001f95f8dd877820b3a5
SayedAnwarOfficial <sayedanwarofficial@gmail.com>
2026-08-19 17:00:27 +0600

    asgi fixing

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,19 +1,20 @@
 import os
 
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "backend.settings",
+)
+
+
 from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter, URLRouter
+
 from apps.users.websocket_auth import (
     JWTAuthMiddlewareStack,
 )
 
 from apps.notifications.routing import websocket_urlpatterns
-
-
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "backend.settings",
-)
 
 
 django_asgi_app = get_asgi_application()
